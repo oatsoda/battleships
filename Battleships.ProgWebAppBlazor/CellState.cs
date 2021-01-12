c08abbf504b8ae5ac607ede77da3fd9a31de5479
oatsoda <andrew+github@milne.tel>
2021-01-12 16:03:56 +0000

    Client Side Blazor Progressive web app beginnings...

---
+++ Battleships.ProgWebAppBlazor/CellState.cs
@@ -0,0 +1,37 @@
+﻿using Battleships.GameEngine;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Battleships.ProgWebAppBlazor
+{
+    public enum CellState
+    {
+        None,
+        PlacedShip,
+        Miss,
+        Hit,
+        Sunk            
+    }
+
+    public class GridState
+    {
+        public Dictionary<Point, CellState> Grid { get; } 
+
+        public GridState()
+        {
+            Grid = new Dictionary<Point, CellState>();
+
+            for (int x = 0; x < 10; x++)
+                for (int y = 0; y < 10; y++)
+                    Grid[new Point(x,y)] = CellState.None;            
+        }
+
+        public void DrawShip(Ship ship)
+        {
+            foreach (var s in ship.Occupies)
+            {
+                Grid[s] = CellState.PlacedShip;
+            }
+        }
+    }
+}
